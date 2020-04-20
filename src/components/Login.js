a18fa538ecec3e36a7e39b1e08f067b9246db4ae
unknown <yalla.venky111@gmail.com>
2020-04-20 14:31:15 +0530

    updated login and app

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,59 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput , View, Button} from 'react-native';
+import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 export default function Login() {
-    return (
-        <View style={styles.container}>
-      <View style={styles.input}><Text style={styles.Text}>UserName: </Text><TextInput style={styles.TextInput} defaultValue="Email/Phone"></TextInput></View>
-      <View style={styles.input}><Text style={styles.Text}>Password: </Text><TextInput style={styles.TextInput} defaultValue="password"></TextInput></View>
-      <View style={styles.input}><Button title='Login'  ></Button></View>
+  return (
+    <View style={styles.container}>
       <View style={styles.input}>
-      <View style={styles.button}><Button  title='Forgot Password'  ></Button></View>
-      <View style={styles.button}><Button style={styles.button} title='Signup'  ></Button></View>
+        <Text style={styles.Text}>UserName: </Text>
+        <TextInput style={styles.TextInput} defaultValue="Email/Phone" />
+      </View>
+      <View style={styles.input}>
+        <Text style={styles.Text}>Password: </Text>
+        <TextInput style={styles.TextInput} defaultValue="password" />
+      </View>
+      <View style={styles.input}>
+        <Button title="Login" />
       </View>
     </View>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection:"column",
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'top',
-      borderRadius:"3px",
-      margin: 40
-    },
-    input:{
-      flexDirection:"row",
-      width: 200,
-      height: 30,
-      justifyContent:'center',
-      margin:2
-    },
-    TextInput:{
-      borderWidth: "1px",
-      borderColor: "gray",
-      margin:"4px",
-      width:120,
-      textAlign: "center"
-    },
-    Text:{
-      width: 80,
-      paddingTop: 5
-    },
-    button:{
-      flexDirection:"row",
-      borderRadius: "3px",
-      width: 100,
-      height: 25,
-      paddingLeft: 40,
-      margin:4,
-      justifyContent:"center",
-      alignContent:"center"
-    }
-  });
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    borderRadius: 1,
+    margin: 40,
+  },
+  input: {
+    flexDirection: 'row',
+    width: 200,
+    height: 30,
+    justifyContent: 'center',
+    margin: 2,
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    margin: 4,
+    width: 120,
+    textAlign: 'center',
+  },
+  Text: {
+    width: 80,
+    paddingTop: 5,
+  },
+  button: {
+    flexDirection: 'row',
+    borderRadius: 3,
+    width: 200,
+    height: 70,
+    paddingLeft: 40,
+    margin: 4,
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
